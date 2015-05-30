77d9ad42b76cd712659a2aa5dd7539726382ccad
Michal Kreft <yomajkel@gmail.com>
2015-05-30 15:50:05 +0200

    extracted config code to config_parser

---
+++ lib/config_parser.rb
@@ -0,0 +1,25 @@
+require 'optparse'
+require 'yaml'
+
+class ConfigParser
+  def self.full_config!
+    options = {}
+	OptionParser.new do |opts|
+	  opts.banner = "Usage: example.rb [options]"
+
+	  opts.on('-f', '--from-version VERSION', 'Release version from which to start') { |v| options[:from_version] = v }
+	  opts.on('-t', '--to-version VERSION', 'Release version to end gathering tickets') { |v| options[:to_version] = v }
+	  opts.on_tail("-h", "--help", "Show this message") { puts opts; exit }
+	  opts.on_tail("--version", "Show version") { puts '0.0.1'; exit }
+
+	end.parse!
+
+	config = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/../config.yaml')
+
+	abort "Missing argument: from version" if options[:from_version].to_s == ''
+	abort "Missing argument: to version"  if options[:to_version].to_s == ''
+	abort "Missing tickets pattern in config file"  if config['tickets_pattern'].to_s == ''
+
+	return options.merge(config)
+  end
+end
--- tickets.rb
+++ tickets.rb
@@ -1,34 +1,18 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
-require 'yaml'
+require_relative 'lib/config_parser'
 require 'colorize'
 require 'pp'
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
-
-  opts.on('-f', '--from-version VERSION', 'Release version from which to start') { |v| options[:from_version] = v }
-  opts.on('-t', '--to-version VERSION', 'Release version to end gathering tickets') { |v| options[:to_version] = v }
-  opts.on_tail("-h", "--help", "Show this message") { puts opts; exit }
-  opts.on_tail("--version", "Show version") { puts '0.0.1'; exit }
-
-end.parse!
-
-config = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/tickets_config.yaml')
-
-abort "Missing argument: from version" if options[:from_version].to_s == ''
-abort "Missing argument: to version"  if options[:to_version].to_s == ''
-abort "Missing tickets pattern in config file"  if config['tickets_pattern'].to_s == ''
+config = ConfigParser.full_config!
 
 branches = `git ls-remote -t -h`
 
 abort "Unable to retrieve branches and tags information from remote" if branches.to_s == ''
 
 regexp_prefix = '([a-zA-Z0-9]{40})\t[a-zA-Z0-9\/]*?\/'
-hash_from = branches[/#{regexp_prefix}#{Regexp.escape(options[:from_version])}/, 1]
-hash_to = branches[/#{regexp_prefix}#{Regexp.escape(options[:to_version])}/, 1]
+hash_from = branches[/#{regexp_prefix}#{Regexp.escape(config[:from_version])}/, 1]
+hash_to = branches[/#{regexp_prefix}#{Regexp.escape(config[:to_version])}/, 1]
 
 abort "Unable to get #{options.from_version} hash from remote." if hash_from.to_s == ''
 abort "Unable to get #{options.to_version} hash from remote." if hash_to.to_s == ''
